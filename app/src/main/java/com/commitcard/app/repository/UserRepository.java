b72a35b2c780b23e424981fb1da93f59ee2ff57d
neyadrian <neyadrian2018@gmail.com>
2026-07-17 13:20:52 -0300

    Criação dos Repositorios para User e Product

--- app/src/main/java/com/commitcard/app/repository/UserRepository.java
+++ app/src/main/java/com/commitcard/app/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package com.commitcard.app.repository;
 
-public class UserRepository {
+import com.commitcard.app.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository <User, Long> {
+
+    Optional<User> findByGithubUsername(String githubUsername);
+
 }
